3f559dd58527531d39a01be89abed9bd27d9ce09
David Diaz <david.diaz@zweicom.com>
2018-10-03 10:38:50 -0300

    modifica style y components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import logo from './../logo.svg';
+
+//Responsive
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+
+import './../styles.css'
+
 
 export default class Header extends React.Component{
 
 	render(){
 	
 		return(
-		
-			<p> HEADER!!!!!!! </p>
-			
+
+			<div >
+				<p>
+					HEADER!!!!!!
+				</p>
+			</div>
+
 		)
 	
 	}
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Header } from './Header'
+export { default as HorizontalMenu } from './HorizontalMenu'
+export { default as Contenido } from './Contenido'
+export { default as Footer } from './Footer'
+
+
