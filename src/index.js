69b82df2fe8dfa4109a701943bbf61dff53692bf
David Diaz <david.diaz@zweicom.com>
2018-10-11 10:51:55 -0300

    import de lib necesarias

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/js/bootstrap.js';
+//import 'react-bootstrap/react-bootstrap.min.js';
+
+
+//import './styles.css'
+
+//import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
+//import 'jquery/dist/jquery.min.js'
+//import 'bootstrap/dist/js/bootstrap.min.js'
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
